3a7a64199f59b58ff20012030c0429e0b3583949
Bryan Foronda <brianforonda94@gmail.com>
2022-06-28 16:20:56 -0500

    CreateTarea

---
+++ src/components/TaskCreator.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const TaskCreator = () => {
+  const [ newTaskName, setNewTaskName ] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem('tasks', newTaskName);
+    setNewTaskName('')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+      type="text" 
+      placeholder="Enter a New Task" 
+      value={newTaskName}
+      onChange={(e) => setNewTaskName(e.target.value)}
+      />
+      <button>Save Task</button>
+    </form>
+  )
+} 
+
+export default TaskCreator
